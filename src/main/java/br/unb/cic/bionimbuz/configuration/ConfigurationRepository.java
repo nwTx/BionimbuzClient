71751b503ae745809ee123fed229185d3f31b11c
tbfreitas <tarcisiodf23@gmail.com>
2015-10-05 14:36:18 -0300

    Configuração necessária para rodar na máquina do LABID

--- src/main/java/br/unb/cic/bionimbuz/configuration/ConfigurationRepository.java
+++ src/main/java/br/unb/cic/bionimbuz/configuration/ConfigurationRepository.java
@@ -24,6 +24,7 @@ import javax.servlet.ServletContextListener;
  */
 @Named
 public class ConfigurationRepository implements ServletContextListener {
+	private static final String CONFIGURATION_FOLDER = "/Users/usuario/Documents/BioNimbuz/projetos/BionimbuzClient/conf/";
 	private static Configuration applicationConfiguration;
 	private static Configuration parallelProgramList;
 	private static Configuration sequentialProgramList;
@@ -37,16 +38,16 @@ public class ConfigurationRepository implements ServletContextListener {
 		System.out.println("Loading configurations...");
 
 		applicationConfiguration = ConfigurationLoader.readConfiguration(
-						"/Users/usuario/Documents/BioNimbuz/projetos/BionimbuzClient/conf/config.json",
+						CONFIGURATION_FOLDER + "config.json",
 						ApplicationConfiguration.class);
 
 		sequentialProgramList = ConfigurationLoader.readConfiguration(
-						"/Users/usuario/Documents/BioNimbuz/projetos/BionimbuzClient/conf/sequential_programs.json",
+						CONFIGURATION_FOLDER + "sequential_programs.json",
 						SequentialProgramList.class);
 
 		parallelProgramList = ConfigurationLoader.readConfiguration(
-				"/Users/usuario/Documents/BioNimbuz/projetos/BionimbuzClient/conf/parallel_programs.json",
-				ParallelProgramList.class);
+						CONFIGURATION_FOLDER + "parallel_programs.json",
+						ParallelProgramList.class);
 		
 		System.out.println("Application Configuration loaded: " + applicationConfiguration);
 		System.out.println("Sequential Programs loaded: " + sequentialProgramList);
