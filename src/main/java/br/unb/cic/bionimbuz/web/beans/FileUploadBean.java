b7f04668597aedde020445d7a650665327a832f5
vramos91 <zoonimbus@zoonimbus-vm>
2015-12-05 22:50:58 -0200

    Finished FileUploadBean

--- src/main/java/br/unb/cic/bionimbuz/web/beans/FileUploadBean.java
+++ src/main/java/br/unb/cic/bionimbuz/web/beans/FileUploadBean.java
@@ -20,6 +20,7 @@ import org.primefaces.event.FileUploadEvent;
 import br.unb.cic.bionimbuz.configuration.ConfigurationRepository;
 import br.unb.cic.bionimbuz.model.UploadedFileInfo;
 import br.unb.cic.bionimbuz.rest.service.RestService;
+import java.math.BigDecimal;
 import javax.faces.application.FacesMessage.Severity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +29,7 @@ import org.slf4j.LoggerFactory;
 @SessionScoped
 public class FileUploadBean implements Serializable {
 
-    private static final Long MAX_STORAGE_SIZE = 268435456L;    // 256 Mb
+    private static final Long MAX_STORAGE_SIZE = 268435456l;    // 256 Mb
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadBean.class);
 
@@ -76,7 +77,7 @@ public class FileUploadBean implements Serializable {
         } catch (Exception e) {
             showFacesMessage(FacesMessage.SEVERITY_ERROR, "Erro Interno. Não foi possível enviar o arquivo");
 
-            e.printStackTrace();
+            LOGGER.error("[Exception - " + e.getMessage() + "]");
             return;
 
         } finally {
@@ -84,16 +85,17 @@ public class FileUploadBean implements Serializable {
                 outputStream.flush();
                 //    outputStream.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.error("[IOException - " + e.getMessage() + "]");
             }
         }
 
         // If file was uploaded with success, adds file on user file list
         sessionBean.getLoggedUser().getFiles().add(fileInfo);
-        
+
         // Adds the size of the uploaded file to the user storage usage
         sessionBean.getLoggedUser().addStorageUsage(event.getFile().getSize());
 
+        // Shows message to the user
         showFacesMessage(FacesMessage.SEVERITY_INFO, "Arquivo enviado com sucesso!");
     }
 
@@ -121,7 +123,7 @@ public class FileUploadBean implements Serializable {
                     + fileName + "]");
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            LOGGER.error("[IOException - " + e.getMessage() + "]");
         }
     }
 
@@ -131,10 +133,16 @@ public class FileUploadBean implements Serializable {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
+    // Retrieves user's storage usage
     public int getStorageUsage() {
-        Long t = (sessionBean.getLoggedUser().getStorageUsage() / MAX_STORAGE_SIZE ) * 100;
-        
-        return t.intValue();
+        // Gets user storage usage
+        BigDecimal usage = new BigDecimal(sessionBean.getLoggedUser().getStorageUsage());
+
+        /**
+         * Returns the user storage usage in percentual. returns int value
+         * because PrimeFaces component accepts only int value from 0 to 100
+         */
+        return usage.divide(new BigDecimal(MAX_STORAGE_SIZE)).multiply(new BigDecimal(100)).intValue();
     }
 
 }
