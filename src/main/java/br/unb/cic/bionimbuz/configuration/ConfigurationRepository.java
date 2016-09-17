82d1242dacea3d03003b304eb45c1f7e861e5563
Jefferson Chaves Gomes <jefferson.chaves@gmail.com>
2016-09-17 13:35:53 -0300

    Removing unsed varialble. Refactor

--- src/main/java/br/unb/cic/bionimbuz/configuration/ConfigurationRepository.java
+++ src/main/java/br/unb/cic/bionimbuz/configuration/ConfigurationRepository.java
@@ -1,19 +1,22 @@
 package br.unb.cic.bionimbuz.configuration;
 
-import br.unb.cic.bionimbuz.model.PluginService;
-import br.unb.cic.bionimbuz.rest.response.GetConfigurationsResponse;
-import br.unb.cic.bionimbuz.rest.service.RestService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
 import javax.inject.Named;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.swing.filechooser.FileSystemView;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import br.unb.cic.bionimbuz.model.PluginService;
+import br.unb.cic.bionimbuz.rest.response.GetConfigurationsResponse;
+import br.unb.cic.bionimbuz.rest.service.RestService;
 
 /**
  * Class that acts as a repository for all Configuration Files.
@@ -26,80 +29,56 @@ import org.slf4j.LoggerFactory;
 public class ConfigurationRepository implements ServletContextListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationRepository.class);
-
-    private static final BionimbuzClientConfig config = loadConfiguration("conf.yaml");
-
-    public static final String ROOT_PATH = config.getRootPath();
-
-    public static final String TEMPORARY_WORKFLOW_PATH = config.getTemporaryWorkflowFolder();
-
-    public static final String ADDRESS = config.getAddress();
-
-    public static final String BIONIMBUZ_ADDRESS = config.getBionimbuzAddress();
-
+    private static BionimbuzClientConfig config;
     private static ArrayList<PluginService> supportedServices;
-
     private static ArrayList<String> references;
-
     private static ArrayList<String> supportedFormats;
+    public static String BIONIMBUZ_ADDRESS;
+    public static String TEMPORARY_WORKFLOW_PATH;
 
     /**
      * Called on Application Server start
      *
-     * @param servletContext
+     * @param event
      */
     @Override
-    public void contextInitialized(ServletContextEvent servletContext) {
-        boolean serverOnline = false;
-        RestService restService = new RestService();
-
+    public void contextInitialized(ServletContextEvent event) {
         LOGGER.info("========================================");
         LOGGER.info("========> Starting client application...");
         LOGGER.info("========================================");
-
-        // Log configurations
+        boolean serverOnline = false;
+        final RestService restService = new RestService();
+        config = loadConfiguration();
         config.log();
-
         // Send request to the server
-        do {
+        while (!serverOnline) {
             if (restService.ping(config.getBionimbuzAddress())) {
                 GetConfigurationsResponse response;
-
                 try {
                     response = restService.getServices();
-
                     references = (ArrayList<String>) response.getReferences();
                     supportedServices = (ArrayList<PluginService>) response.getServicesList();
                     supportedFormats = (ArrayList<String>) response.getSupportedFormats();
-                } catch (Exception ex) {
+                } catch (final Exception ex) {
                     ex.printStackTrace();
                 }
-
             } else {
-
                 LOGGER.error("===> BioNimbuZ Core Offline... Trying reconnection <===");
-
-                // Wait 10 seconds to try again
                 try {
-                    Thread.sleep(10000l);
-
-                } catch (InterruptedException ex) {
+                    // Wait 30 seconds to try again
+                    Thread.sleep(30000l);
+                } catch (final InterruptedException ex) {
                     LOGGER.error("[InterruptedException] " + ex.getMessage());
                 }
             }
-
-            serverOnline = (supportedServices != null);
-        } while (!serverOnline);
-
+            serverOnline = supportedServices != null;
+        }
         LOGGER.info(supportedServices.size() + " Supported Services");
         LOGGER.info("Supported Services fetched from server: ");
-
-        for (PluginService p : supportedServices) {
+        for (final PluginService p : supportedServices) {
             LOGGER.info(" - Program: " + p.getName());
         }
-
     }
-
     /**
      * Called on Application Server stop
      *
@@ -111,23 +90,18 @@ public class ConfigurationRepository implements ServletContextListener {
         LOGGER.info("========> Stopping client application...");
         LOGGER.info("========================================");
     }
-
     public static ArrayList<PluginService> getSupportedServices() {
         return supportedServices;
     }
-
     public static ArrayList<String> getReferences() {
         return references;
     }
-
     public static ArrayList<String> getSupportedFormats() {
         return supportedFormats;
     }
-
     public static BionimbuzClientConfig getConfig() {
         return config;
     }
-
     /**
      * Loads web application configuration.
      *
@@ -135,18 +109,15 @@ public class ConfigurationRepository implements ServletContextListener {
      * @return
      * @throws IOException
      */
-    private static BionimbuzClientConfig loadConfiguration(final String filename) {
-
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    private static BionimbuzClientConfig loadConfiguration() {
         BionimbuzClientConfig config = null;
-
         try {
-            config = mapper.readValue(new File(FileSystemView.getFileSystemView().getHomeDirectory() + "/BionimbuzClient/conf/conf.yaml"), BionimbuzClientConfig.class);
-        } catch (IOException ex) {
-            LOGGER.error("[IOExceptin] - " + ex.getMessage());
+            final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            final String defaultConfigPathname = System.getProperty("user.home") + "/BionimbuzClient/conf/conf.yaml";
+            config = mapper.readValue(new File(defaultConfigPathname), BionimbuzClientConfig.class);
+        } catch (final IOException ex) {
+            LOGGER.error("[IOException] - " + ex.getMessage());
         }
-
         return config;
     }
-
 }
--- src/main/java/br/unb/cic/bionimbuz/web/beans/SessionBean.java
+++ src/main/java/br/unb/cic/bionimbuz/web/beans/SessionBean.java
@@ -1,40 +1,37 @@
 package br.unb.cic.bionimbuz.web.beans;
 
 import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.unb.cic.bionimbuz.exception.ServerNotReachableException;
 import br.unb.cic.bionimbuz.model.User;
 import br.unb.cic.bionimbuz.rest.service.RestService;
 import br.unb.cic.bionimbuz.security.PBKDF2;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Named
 @SessionScoped
 public class SessionBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(SessionBean.class);
-
     private final RestService restService;
-
-    private User loggedUser = new User();
-
     private boolean serverStatus;
-
+    private User loggedUser = new User();
     private User user = new User();
 
     // Inicializes session
     public SessionBean() {
-        restService = new RestService();
+        this.restService = new RestService();
     }
-
     /**
      * Connect to the server to execute a Login request
      *
@@ -43,47 +40,38 @@ public class SessionBean implements Serializable {
     public String login() throws NoSuchAlgorithmException, InvalidKeySpecException {
         String password = "";
         User responseUser = null;
-//        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-//        String ipAddres =request.getHeader("X-FORWARDED-FOR");
-//    
-//        if(ipAddres == null){
-//            ipAddres=request.getRemoteAddr();
-//        }
-//        System.out.println("ip: "+ipAddres);
-
+        // HttpServletRequest request = (HttpServletRequest)
+        // FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        // String ipAddres =request.getHeader("X-FORWARDED-FOR");
+        //
+        // if(ipAddres == null){
+        // ipAddres=request.getRemoteAddr();
+        // }
+        // System.out.println("ip: "+ipAddres);
         /**
          * Hashes password using bCrypt algorithm with computational complexity
          * of 13.
          */
-        
         try {
-            password = user.getPassword();
-            user.setPassword("");
-            
-            responseUser = restService.login(user);
-            
-        } catch (Exception e) {
+            password = this.user.getPassword();
+            this.user.setPassword("");
+            responseUser = this.restService.login(this.user);
+        } catch (final Exception e) {
             LOGGER.error("Server is offline (or HTTP 500 - Internal error)");
             e.printStackTrace();
-
             return "login?faces-redirect=true&internal_error=true";
         }
-
-        // If user.cpf is not null, user sent to the server was found on database and had its data retrieved back to this client
-        if (responseUser.getCpf() != null && (PBKDF2.validatePassword(password, responseUser.getPassword()))) {
-            loggedUser = responseUser;
-
+        // If user.cpf is not null, user sent to the server was found on
+        // database and had its data retrieved back to this client
+        if (responseUser.getCpf() != null && PBKDF2.validatePassword(password, responseUser.getPassword())) {
+            this.loggedUser = responseUser;
             return "success";
-
-            // Login not found 
         } else {
-            user = new User();
-
+            // Login not found
+            this.user = new User();
             return "login?faces-redirect=true&not_found=true";
         }
-
     }
-
     /**
      * Invalidates user session and communicates to the server
      *
@@ -91,40 +79,37 @@ public class SessionBean implements Serializable {
      */
     public String logout() {
         // Invalidates user session
-        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-
+        final FacesContext currentInstance = FacesContext.getCurrentInstance();
+        if (currentInstance != null) {
+            final ExternalContext externalContext = currentInstance.getExternalContext();
+            if (externalContext != null) {
+                externalContext.invalidateSession();
+            }
+        }
         // Send a logout message to the server
         try {
-            restService.logout(loggedUser);
-        } catch (Exception e) {
-            e.printStackTrace();
+            this.restService.logout(this.loggedUser);
+        } catch (final ServerNotReachableException e) {
+            LOGGER.error("Server logout error", e);
         }
-
         // Reset user variables
         this.loggedUser = new User();
         this.user = new User();
-
         return "logout";
     }
-
     public User getUser() {
-        return user;
+        return this.user;
     }
-
     public void setUser(User user) {
         this.user = user;
     }
-
     public User getLoggedUser() {
-        return loggedUser;
+        return this.loggedUser;
     }
-
     public boolean isServerStatus() {
-        return serverStatus;
+        return this.serverStatus;
     }
-
     public void setServerStatus(boolean serverStatus) {
         this.serverStatus = serverStatus;
     }
-
 }
