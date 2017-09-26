239142452a6e0d39c029e725b034a86edd2df117
Guilherme Vergara <gfv.unb@gmail.com>
2017-09-25 21:33:45 -0300

    Merge

--- src/main/java/br/unb/cic/bionimbuz/elasticity/AmazonAPI.java
+++ src/main/java/br/unb/cic/bionimbuz/elasticity/AmazonAPI.java
@@ -1,209 +1,193 @@
 package br.unb.cic.bionimbuz.elasticity;
 
-import br.unb.cic.bionimbuz.elasticity.legacy.Ec2Commands;
-import static br.unb.cic.bionimbuz.elasticity.legacy.Ec2Commands.EC2;
-import static br.unb.cic.bionimbuz.elasticity.legacy.Ec2Commands.instanceid;
-import static br.unb.cic.bionimbuz.elasticity.legacy.Ec2Commands.user_input;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.KeyPair;
-import com.amazonaws.services.ec2.model.RunInstancesRequest;
-import com.amazonaws.services.ec2.model.RunInstancesResult;
-import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.model.CreateImageRequest;
 import com.amazonaws.services.ec2.model.CreateImageResult;
-import com.amazonaws.services.ec2.model.CreateSnapshotRequest;
 import com.amazonaws.services.ec2.model.CreateTagsRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.KeyPair;
 import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
 import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 
-import java.io.*;
-import java.util.*;
-
-public class AmazonAPI  {
-
+public class AmazonAPI {
+    
     public static AmazonEC2 EC2;
     public static Scanner user_input = new Scanner(System.in);
     public static String instanceid;
     public static KeyPair keyPair;
     public static int count = 1;
     private String ipInstance;
-
+    
     public void setup() {
         try {
-            String credentialsFile = System.getProperty("user.home") + "/BionimbuzClient/target/BionimbuzClient-0.0.1-SNAPSHOT/resources/apiCredentials/AwsCredentials.properties";
+            final String credentialsFile = "/opt/shared/BionimbuzClient/target/BionimbuzClient-0.0.1-SNAPSHOT/resources/apiCredentials/AwsCredentials.properties";
             InputStream is = null;
             is = new FileInputStream(credentialsFile);
-            PropertiesCredentials credentials = new PropertiesCredentials(is);
-
+            final PropertiesCredentials credentials = new PropertiesCredentials(is);
+            
             EC2 = new AmazonEC2Client(credentials);
             EC2.setEndpoint("ec2.sa-east-1.amazonaws.com");
-
-        } catch (IOException ioe) {
+            
+        } catch (final IOException ioe) {
             throw new RuntimeException(ioe);
-        }//, IllegalArgumentException
+        }// , IllegalArgumentException
     }
-
+    
     public String createinstance(String type, String imageID) throws IOException {
-        // Acho que tem que mudar isso aqui em    
-        //imageID = "ami-912db2fd";
+        // Acho que tem que mudar isso aqui em
+        // imageID = "ami-912db2fd";
         this.setup();
         String IP = null;
         try {
-
+            
             // CREATE EC2 INSTANCES
-            RunInstancesRequest runInstancesRequest = new RunInstancesRequest()
-                    .withInstanceType(type)
-                    .withImageId(imageID)
-                    .withMinCount(1)
-                    .withMaxCount(1)
-                    .withSecurityGroupIds("default");
+            final RunInstancesRequest runInstancesRequest = new RunInstancesRequest().withInstanceType(type).withImageId(imageID).withMinCount(1).withMaxCount(1).withSecurityGroupIds("default");
             
             runInstancesRequest.setMonitoring(Boolean.TRUE);
             
-            //.withKeyName("xebia-france")
-            //.withUserData(Base64.encodeBase64String(myUserData.getBytes()))
-
-//            System.out.println("Criando nova maquina BioninbuZ");
-//            //String imageId = "ami-6e3bb102";
-//            String imageId = "ami-912db2fd";
-//
-//            int minInstanceCount = 1; // 
-//            int maxInstanceCount = 1;
-//            RunInstancesRequest rir = new RunInstancesRequest(imageId, minInstanceCount, maxInstanceCount);
-//            rir.setInstanceType(type);
-//            rir.withSecurityGroups("default");
-//            //rir.setMonitoring(Boolean.TRUE);
-            //RunInstancesResult result = AmazonAPI.EC2.runInstances(rir);
-            RunInstancesResult result = AmazonAPI.EC2.runInstances(runInstancesRequest);
-
+            // .withKeyName("xebia-france")
+            // .withUserData(Base64.encodeBase64String(myUserData.getBytes()))
+            
+            // System.out.println("Criando nova maquina BioninbuZ");
+            // //String imageId = "ami-6e3bb102";
+            // String imageId = "ami-912db2fd";
+            //
+            // int minInstanceCount = 1; //
+            // int maxInstanceCount = 1;
+            // RunInstancesRequest rir = new RunInstancesRequest(imageId, minInstanceCount, maxInstanceCount);
+            // rir.setInstanceType(type);
+            // rir.withSecurityGroups("default");
+            // //rir.setMonitoring(Boolean.TRUE);
+            // RunInstancesResult result = AmazonAPI.EC2.runInstances(rir);
+            final RunInstancesResult result = AmazonAPI.EC2.runInstances(runInstancesRequest);
+            
             System.out.println("waiting");
-                        
+            
             System.out.println("OK");
-
-            List<com.amazonaws.services.ec2.model.Instance> resultInstance = result.getReservation().getInstances();
-
+            
+            final List<com.amazonaws.services.ec2.model.Instance> resultInstance = result.getReservation().getInstances();
+            
             String createdInstanceId = null;
             int idx = 1;
             
-            for (com.amazonaws.services.ec2.model.Instance ins : resultInstance) {
-
-                createdInstanceId = ins.getInstanceId();
-
-                System.out.println("New instance has been created: " + createdInstanceId);//print the instance ID
+            for (final com.amazonaws.services.ec2.model.Instance ins : resultInstance) {
                 
-                CreateTagsRequest createTagsRequest = new CreateTagsRequest();
+                createdInstanceId = ins.getInstanceId();
+                
+                System.out.println("New instance has been created: " + createdInstanceId);// print the instance ID
+                
+                final CreateTagsRequest createTagsRequest = new CreateTagsRequest();
                 createTagsRequest.withResources(createdInstanceId) //
-                .withTags(new Tag("Name", "BioNimbuZ-" + idx));
+                        .withTags(new Tag("Name", "BioNimbuZ-" + idx));
                 EC2.createTags(createTagsRequest);
-
+                
                 idx++;
-
-                DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds(ins.getInstanceId());
-                DescribeInstancesResult result2 = EC2.describeInstances(request);
-                List<Reservation> list = result2.getReservations();
-            
-                long startTime = System.currentTimeMillis();    
-            while (IP == null){
-                for (Reservation res : list) {
-                    List<com.amazonaws.services.ec2.model.Instance> instanceList = res.getInstances();
-
-                    for (com.amazonaws.services.ec2.model.Instance instance : instanceList) {
+                
+                final DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds(ins.getInstanceId());
+                final DescribeInstancesResult result2 = EC2.describeInstances(request);
+                final List<Reservation> list = result2.getReservations();
+                
+                final long startTime = System.currentTimeMillis();
+                while (IP == null) {
+                    for (final Reservation res : list) {
+                        final List<com.amazonaws.services.ec2.model.Instance> instanceList = res.getInstances();
                         
-                        IP = instance.getPublicIpAddress();
-                        
-                        
-                        setIpInstance(instance.getPublicIpAddress());
+                        for (final com.amazonaws.services.ec2.model.Instance instance : instanceList) {
+                            
+                            IP = instance.getPublicIpAddress();
+                            
+                            this.setIpInstance(instance.getPublicIpAddress());
+                        }
                     }
                 }
+                final long estimatedTime = System.currentTimeMillis() - startTime;
+                
+                System.out.println("Instance Public IP :" + IP + " com tempo:" + estimatedTime);
+                
             }
-            long estimatedTime = System.currentTimeMillis() - startTime;
-            
-            System.out.println("Instance Public IP :" + IP + " com tempo:" + estimatedTime);
-            
-            
-            
-            }
-        } catch (AmazonServiceException ase) {
+        } catch (final AmazonServiceException ase) {
             System.out.println("Caught Exception: " + ase.getMessage());
             System.out.println("Reponse Status Code: " + ase.getStatusCode());
             System.out.println("Error Code: " + ase.getErrorCode());
             System.out.println("Request ID: " + ase.getRequestId());
             System.out.println("Give a valid input");
             System.out.println("");
-//		AmazonAPI.enteroption();
+            // AmazonAPI.enteroption();
         }
         return IP;
     }
-
+    
     public static List<com.amazonaws.services.ec2.model.Instance> listinstances() {
-        AmazonAPI api = new AmazonAPI();
+        final AmazonAPI api = new AmazonAPI();
         api.setup();
-
-        DescribeInstancesResult describeInstancesRequest = EC2.describeInstances();
-        List<Reservation> reservations = describeInstancesRequest.getReservations();
-        List<com.amazonaws.services.ec2.model.Instance> instances = new ArrayList<>();
-
-        for (Reservation reservation : reservations) {
+        
+        final DescribeInstancesResult describeInstancesRequest = EC2.describeInstances();
+        final List<Reservation> reservations = describeInstancesRequest.getReservations();
+        final List<com.amazonaws.services.ec2.model.Instance> instances = new ArrayList<>();
+        
+        for (final Reservation reservation : reservations) {
             instances.addAll(reservation.getInstances());
         }
-
+        
         return instances;
-
+        
     }
-
+    
     public String getIpInstance() {
-        return ipInstance;
+        return this.ipInstance;
     }
-
+    
     public void setIpInstance(String ipInstance) {
         this.ipInstance = ipInstance;
     }
     
-    public void terminate(String instanceid)  {
+    public void terminate(String instanceid) {
         this.setup();
-        TerminateInstancesRequest tir = new TerminateInstancesRequest()
-                .withInstanceIds(instanceid);
+        final TerminateInstancesRequest tir = new TerminateInstancesRequest().withInstanceIds(instanceid);
         EC2.terminateInstances(tir);
         System.out.println("Terminating the instance : " + instanceid);
     }
-
     
     public String createami(String instanceid) throws IOException {
         this.setup();
-
-            //CreateSnapshotRequest ssr = new CreateSnapshotRequest(instanceid, instanceid);
-            CreateImageRequest cir = new CreateImageRequest(instanceid, instanceid);
-            CreateImageResult cires = EC2.createImage(cir);
-            String imageid;
-            imageid = cires.getImageId();
-            String imageID = cires.getImageId();
-            System.out.println("The imageid of the newly created AMI is " + imageid);
-
-            try {
-                Thread.sleep(120000);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        
+        // CreateSnapshotRequest ssr = new CreateSnapshotRequest(instanceid, instanceid);
+        final CreateImageRequest cir = new CreateImageRequest(instanceid, instanceid);
+        final CreateImageResult cires = EC2.createImage(cir);
+        String imageid;
+        imageid = cires.getImageId();
+        final String imageID = cires.getImageId();
+        System.out.println("The imageid of the newly created AMI is " + imageid);
+        
+        try {
+            Thread.sleep(120000);
+        } catch (final InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return imageID;
     }
-            
-    public void executeElasticity(String id) throws IOException        {
+    
+    public void executeElasticity(String id) throws IOException {
         this.setup();
-        //this.createami(id);
+        // this.createami(id);
         this.createinstance("t2.micro", this.createami(id));
         this.terminate(id);
         
-    
     }
-
-
     
-    
-
-} //main end
+} // main end
